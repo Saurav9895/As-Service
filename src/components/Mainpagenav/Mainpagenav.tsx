653f077145d8b09bbfe3e94ba02733185446c805
Saurav Yadav <93671987+Saurav9895@users.noreply.github.com>
2024-03-16 03:19:02 +0530

    changes made

--- src/components/Mainpagenav/Mainpagenav.tsx
+++ src/components/Mainpagenav/Mainpagenav.tsx
@@ -18,6 +18,7 @@ const Mainpagenav:React.FC<MainpagenavProps> = () => {
 
 
     return (
+        <>
         <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-green text-dark-gray-7'>
 			<div className="flex w-full items-center justify-between max-w-[1200px] mx-auto">
         <Link href='/' className='h-[22px] flex-1'>
@@ -75,13 +76,12 @@ const Mainpagenav:React.FC<MainpagenavProps> = () => {
 						</div>
 					)} 
               		{user && <Logout/>}
-        
 
-                                  
-        
         </div>
     </div>
 </nav>
+
+</>
     );
 }
 export default Mainpagenav;
