e1a129833a51663896fc2e0800dcc084ec24337e
Saurav Yadav <93671987+Saurav9895@users.noreply.github.com>
2024-03-16 22:10:40 +0530

    made changes in mobile responsive

--- src/pages/Serviceperson.tsx
+++ src/pages/Serviceperson.tsx
@@ -55,20 +55,20 @@ const Serviceperson:React.FC<ServicepersonProps> = () => {
         
         <main className="h-full w-full bg-white py-8 sm:px-32 lg:px-40 px-4">
             <div className="bg-white pb-[53px]">
-                <div className="w-full h-[193px] bg-gradient-to-r from-pink-300 via-pink-100 to-yellow-100" />
+                <div className="w-full h-[193px] bg-gradient-to-r from-light-green via-light-idx to-light-green" />
                 <div className="flex justify-between flex-row pt-[44px] items-center -mt-20 pl-[32px]">
-                    <div className="flex flex-row items-center gap-[31px]">
+                    <div className="detailsname">
                         <div className="relative bg-white w-[158px] h-[158px] border border-gray-200 rounded-full ">
                         <Image src="/avatar.png" alt="..." className="avatar-img rounded-circle " height={500} width={500}/>
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col mt-3'>
                             <span className="truncate font-medium text-[22px] text-[#363A43]">{serviceDetails.Fname + (serviceDetails.Mname ? ' ' + serviceDetails.Mname : '') + ' ' + serviceDetails.Lname}</span>
-                            <div className="truncate text-[19px] text-[#363A43]">{serviceDetails.service} </div>
+                            <div className="servicedetails">{serviceDetails.service} </div>
                         </div>
                     </div>
-                    <button className='h-[45px] bg-[#FFFFFF] border border-[#E3E3E3] font-bold text-[12px] text-[#1C274C] rounded-[7px] px-8 py-2 hover:bg-[#FCFCFC] hover:border-[#D9D9D9]'>
+                    {/* <button className='h-[45px] bg-[#FFFFFF] border border-[#E3E3E3] font-bold text-[12px] text-[#1C274C] rounded-[7px] px-8 py-2 hover:bg-[#FCFCFC] hover:border-[#D9D9D9]'>
                         Follow
-                    </button>
+                    </button> */}
                 </div>
             </div>
             <li className="w-full items-center border border-gray-200 hover:bg-[#FCFCFC] h-[65px] flex-1 bg-white px-[20px] flex-col flex justify-between md:flex-row gap-[10px] md:gap-[50px]">
